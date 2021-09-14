a719760f2c130d4ca438ac26ec020ddf9862785b
micha <michaelholliman@btopenworld.com>
2021-09-14 01:23:49 +0100

    Version 1.1 (Added comments)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,23 +9,23 @@ public class Main {
         int finalNumber = 0;
         while (true) {
             System.out.println("Please enter an integer. Type '-1' to exit.");
-            userInput = scanner.nextInt();
-            int tempNumber = userInput;
+            userInput = scanner.nextInt(); // Get user input here.
             List<Integer> numberList = new ArrayList<>();
-            while (tempNumber > 0) {
-                numberList.add( tempNumber % 10 );
-                tempNumber = tempNumber / 10;
+            while (userInput > 0) {
+                numberList.add( userInput % 10 ); // The remainder of any number divided by 10 is the last digit.
+                userInput = userInput / 10; // Divide number by 10, removing the last digit. The result has to be an integer, so the result is rounded.
             }
+            // At this point numberList has each digit of userInput as a separate element in the list.
             for (Integer integer : numberList) {
-                finalNumber += integer;
+                finalNumber += integer; // Add all numbers into one last int.
             }
-            if (userInput == -1) {
+            if (userInput == -1) { // Exit program if the user types "-1".
                 System.out.println("Exiting program...");
                 break;
             }
             else {
                 System.out.println("The sum of the digits of the integer is: " + finalNumber);
-                finalNumber = 0;
+                finalNumber = 0; // Reset number for the next round.
             }
         }
     }
